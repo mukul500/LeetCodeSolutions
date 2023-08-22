c2233e3b71a6f7f1541b8c921d952b83ff69bb02
mukulbanga <mukul.banga001@gmail.com>
2023-08-22 16:29:04 +1000

    Updated Two Sum

--- src/main/kotlin/array/TwoSum.kt
+++ src/main/kotlin/array/TwoSum.kt
@@ -3,16 +3,24 @@ package array
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
 
+        //We create a map of Int and Int
+        //Where the key is the number and the value is the index
 
-        //Traverse the Complete Array
-        for(i in nums.indices){
-            //Traverse the Array after the Index i
-            for(j in i+1 until nums.size){
-                //Check if the sum of the two numbers is equal to the target
-                if(nums[i]+nums[j]==target){
-                    return intArrayOf(i,j)
-                }
+        val map = mutableMapOf<Int, Int>()
+
+        //We traverse the array
+        for (i in nums.indices) {
+
+            //We calculate the difference between the target and the current number
+            val diff = target - nums[i]
+
+            //If the difference exists in the map, we return the indices
+            if (map.containsKey(diff)) {
+                //We return the indices
+                return intArrayOf(map[diff]!!, i)
             }
+            //If the difference doesn't exist in the map, we add the number and the index to the map
+            map[nums[i]] = i
         }
         return intArrayOf()
 
