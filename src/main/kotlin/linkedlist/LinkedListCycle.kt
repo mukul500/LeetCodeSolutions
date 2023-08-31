0d1142e8ef2a77e2eee6d529264ef43b9bc67986
mukulbanga <mukul.banga001@gmail.com>
2023-08-31 11:04:01 +1000

    Solved LinkedListCycle

---
+++ src/main/kotlin/linkedlist/LinkedListCycle.kt
@@ -0,0 +1,56 @@
+package linkedlist
+
+class LinkedListCycle {
+
+//
+//    //1st Solution with the Hashset
+//    fun hasCycle(head: ListNode?): Boolean {
+//
+//        //We create a hashset to store the visited nodes
+//        val visitedNodes: HashSet<Int> = hashSetOf()
+//
+//        //We check if the head is null we return false
+//        if (head == null) {
+//            return false
+//        }
+//        var currentNode = head
+//        //We iterate through the list
+//        while (currentNode != null) {
+//            //We check if the current node is in the hashset
+//            if (visitedNodes.contains(currentNode.hashCode())) {
+//                return true
+//            }
+//            //We add the current node to the hashset
+//            visitedNodes.add(currentNode.hashCode())
+//            currentNode = currentNode.next
+//        }
+//        //We return false
+//        return false
+//    }
+
+
+    //2nd Solution with the two pointers Hare and Tortoise Algorithm
+    fun hasCycle(head: ListNode?): Boolean {
+
+
+        //We check if the head is null we return false
+        if (head == null) {
+            return false
+        }
+        var tortoise = head.next
+        var hare = head.next?.next
+        //We iterate through the list
+        while (hare != null && tortoise != null) {
+            //We check if the hare and tortoise are equal
+            if (hare == tortoise) {
+                return true
+            }
+            //We move the tortoise one step
+            tortoise = tortoise.next
+            //We move the hare two steps
+            hare = hare.next?.next
+        }
+        //We return false
+        return false
+    }
+}
--- src/main/kotlin/linkedlist/ReverseLinkedList.kt
+++ src/main/kotlin/linkedlist/ReverseLinkedList.kt
@@ -5,23 +5,35 @@ class ListNode(var `val`: Int) {
     var next: ListNode? = null
 }
 
+
 class ReverseLinkedList {
     fun reverseList(head: ListNode?): ListNode? {
+
+        // We check if the head is null we return null
         if (head == null) {
             return null
         }
+
+        // We create a front node with the head value
+        //We use the Front node as a end node and previous node will have the reference to the next node
         var frontNode = ListNode(head.`val`)
+        // We create a back node with the next value
         var backNode = head.next
 
 
-
+        // We iterate through the list
         while (backNode != null) {
+            // We create a temp node to store the next node
             val tempNode = backNode.next
+            // We set the next node of the back node to the front node
             backNode.next = frontNode
+            // We set the front node to the back node
             frontNode = backNode
+            // We set the back node to the temp node
             backNode = tempNode
         }
 
+        // We return the front node
         return frontNode
     }
 }
