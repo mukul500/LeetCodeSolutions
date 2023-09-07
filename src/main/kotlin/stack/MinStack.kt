47de684ab8145faeed7cf2b544ab165037bdcc84
mukulbanga <mukul.banga001@gmail.com>
2023-09-07 15:28:42 +1000

    Solved Min Stack

---
+++ src/main/kotlin/stack/MinStack.kt
@@ -0,0 +1,79 @@
+package stack
+
+import java.util.*
+
+
+class MinStack {
+
+
+    private val stack: Stack<Int> = Stack()
+    private val treeMap = TreeMap<Int, Int>()
+    fun push(`val`: Int) {
+        stack.push(`val`)
+        treeMap.addNumber(`val`)
+    }
+
+    fun pop() {
+        val number = stack.pop()
+        treeMap.removeNumber(number)
+    }
+
+    fun top(): Int {
+        return stack.peek()
+    }
+
+    fun getMin(): Int {
+        return treeMap.getMinimum()
+    }
+
+    private fun TreeMap<Int, Int>.removeNumber(value : Int){
+        if(this.contains(value)){
+            val currentValue = this[value]
+            if(currentValue!! <= 1 ){
+                this.remove(value)
+            }else{
+                this[value] = currentValue - 1
+            }
+        }
+    }
+
+    private fun TreeMap<Int, Int>.getMinimum() : Int{
+        return this.firstEntry().key
+    }
+
+    private fun TreeMap<Int, Int>.addNumber(value: Int){
+        if(this.contains(value)){
+            val currentValue = this[value]
+            this[value] = currentValue!! + 1
+        }else{
+            this[value] = 1
+        }
+    }
+}
+
+class MinStack2{
+    private val stack: Stack<Int> = Stack()
+    private val minStack: Stack<Int> = Stack()
+
+    fun push(`val`: Int) {
+        stack.push(`val`)
+        if(minStack.isEmpty() || `val` <= minStack.peek()){
+            minStack.push(`val`)
+        }
+    }
+
+    fun pop() {
+        val number = stack.pop()
+        if(number == minStack.peek()){
+            minStack.pop()
+        }
+    }
+
+    fun top(): Int {
+        return stack.peek()
+    }
+
+    fun getMin(): Int {
+        return minStack.peek()
+    }
+}
